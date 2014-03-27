265925312350c32cba15977b9813417f841089b9
zangzhe <zang_zhe@163.com>
2014-03-27 13:37:42 +0800

    增加 douban 测试账户

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,5 @@
 LOG_NAME = 'test.log'  # set the log file name
 
-
-KEY = '03f6dafdd0a1663d0be490f971e432b7' # douban key
-SECRET = '630636a1f2039a67'  # douban secret key
-CALLBACK = 'http://www.baidu.com' # callback url
-TOKEN = '6bbd8428d724dedb43388c7a856e9c91' # access token
-
 #KEY = '011817511e094d62034845f35a1be826'
 #SECRET = 'ed55f78fe13b7538'
 #CALLBACK = 'http://www.baidu.com'
@@ -15,3 +9,18 @@ TOKEN = '6bbd8428d724dedb43388c7a856e9c91' # access token
 #SECRET = 'e3d12f3b7fdb98a0'
 #CALLBACK = 'http://www.baidu.com'
 #TOKEN = '572e7264879a39b7058b572d96cead25'
+
+#KEY = '001b3ec573762ea20051bc505a628be6'
+#SECRET = '5df2dc64bf512350'
+#CALLBACK = 'http://www.baidu.com'
+#TOKEN = '75308561cad0528e5f9190256070f7aa'
+
+KEY = '08d2d73edd3ce5961a7885bc97b6b0c2'
+SECRET = '9dcca0504102bc9c'
+CALLBACK = 'http://www.baidu.com'
+TOKEN = '750529ecc0feb97f5f0f7dc642fc2c7d'
+
+#KEY = '03f6dafdd0a1663d0be490f971e432b7' # douban key
+#SECRET = '630636a1f2039a67'  # douban secret key
+#CALLBACK = 'http://www.baidu.com' # callback url
+#TOKEN = '6bbd8428d724dedb43388c7a856e9c91' # access token
