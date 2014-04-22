0608c1a1301f6b82498722dd657513ea18c92cca
zangzhe <zang_zhe@163.com>
2014-04-22 14:12:32 +0800

    通过超时时间，使用 refresh code，实现自动更新 token的功能

--- tests/framework.py
+++ tests/framework.py
@@ -5,6 +5,7 @@ import sys
 
 from six import print_
 from six.moves import input, reduce
+import ConfigParser, pdb, time
 
 status_code = {'OK':200, 'CREATED':201, 'ACCEPTED':202, 'BAD_REQUEST':400,\
 'UNAUTHORIZED':401, 'FORBIDDEN':403, 'NOT_FOUND':404, 'INTERNAL_SERVER_ERROR':500}
@@ -21,25 +22,36 @@ from douban_client.api.error import DoubanAPIError
 SCOPE_MAP = { 'basic': ['douban_basic_common', 'community_basic_user'], }
 SCOPE = ','.join(reduce(lambda x, y: x + y, SCOPE_MAP.values()))
 
-
-try:
-    from test_config import KEY, SECRET, CALLBACK, TOKEN, LOG_NAME
-except ImportError:
-    KEY = ''
-    SECRET = ''
-    CALLBACK = ''
-    TOKEN = ''
-    LOG_NAME = 'test.log'
+config=ConfigParser.ConfigParser()  
+with open('test_config.cfg', 'r+') as cfgfile:  
+    config.readfp(cfgfile)  
+    
+    log_name = config.get('INFO', 'log_name')
+    key = config.get('INFO', 'key')
+    secret = config.get('INFO', 'secret')
+    callback = config.get('INFO', 'callback')
+    token = config.get('INFO', 'token')
+    refresh_token = config.get('INFO', 'refresh_token')
+    expires_at = config.get('INFO', 'expires_at')
 
 def get_client_v2():
-    client = DoubanClient(KEY, SECRET, CALLBACK, SCOPE)
-
-    token = TOKEN
+    client = DoubanClient(key, secret, callback, SCOPE)
+    if expires_at:
+        expires_time = int(expires_at)
 
     if token:
-        client.auth_with_token(token)
-        #refresh_token_code = REFRESH_TOKEN
-        #client.refresh_token(refresh_token_code)
+        now_time = int(time.time())
+        remaining_seconds = expires_time - now_time
+        print 'sss: ' + str(remaining_seconds)
+        if remaining_seconds > 3600:
+            client.auth_with_token(token)
+        else:
+            client.refresh_token(refresh_token)   
+            config.set('INFO', 'token', client.token_code)
+            config.set('INFO', 'refresh_token', client.refresh_token_code)
+            config.set('INFO', 'expires_at', client.expires_at)
+            with open('test_config.cfg', 'wb') as configfile:
+                config.write(configfile)
     else:
         print_('Go to the following link in your browser:')
         print_(client.authorize_url)
@@ -48,10 +60,19 @@ def get_client_v2():
         client.auth_with_code(code)
         print_('token code:', client.token_code)
         print_('refresh token code:', client.refresh_token_code)
+        print_('expires at:', client.expires_at)
+        print type(client.expires_at)
+        config.set('INFO', 'token', client.token_code)
+        config.set('INFO', 'refresh_token', client.refresh_token_code)
+        config.set('INFO', 'expires_at', client.expires_at)
+        with open('test_config.cfg', 'wb') as configfile:
+            config.write(configfile)
+        #pdb.set_trace()
+
     return client
 
 def test_runner():
-    log_fd = open(LOG_NAME, 'w')
+    log_fd = open(log_name, 'w')
     runner = TextTestRunner(stream=log_fd)
     return runner
 
